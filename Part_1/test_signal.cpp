d81751dcd1bdabaeea198cd13976de3029bf978f
Ben <007930709@coyote.csusb.edu>
2023-10-08 16:20:49 -0700

    brief changes

--- Part_1/test_signal.cpp
+++ Part_1/test_signal.cpp
@@ -5,13 +5,24 @@
 using namespace std;
 void func (int sig){
 	cout << "Received a signal from your program " << sig << endl;
+	if(sig == SIGQUIT){
+	exit(0);
+	}
 }
 int main(){
-	(void) signal (SIGINT, func);//catch terminal interrupts
+	struct sigaction sa;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = func;
+
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
 	
-	for (int i = 0; i < 20; ++i) {
-		cout << "CSUSB CSE 4600 Operating Systems Fall 2023" << endl;
-		sleep (1);
+	cout << "Press Ctrl+C for SIGINT. Press ^\\ (Ctrl+\\) to quit." << endl;
+	
+	while(1) {
+	cout << "CSUSB CSE 4600 Operating Systems Fall 2023" << endl;
+	sleep(1);
 	}
 
 	return 0;
