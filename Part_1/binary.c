866320794108b335ee91bdc8e36977294ebb2852
ladalar <ladanyif97@gmail.com>
2023-10-12 13:41:30 -0700

    <updated partial_sum>

--- Part_1/binary.c
+++ Part_1/binary.c
@@ -3,19 +3,9 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
 int main(int argc, char **argv){
 	pid_t child_pid, parent_pid;
-	
-	printf("Create 3 processes from the same parent process:\n");
-	for (int i = 0; i < 5; i++){
-		if (fork() == 0) {
-			child_pid = getpid();
-			parent_pid = getppid();
-			printf("Child Process (ID): %d, parent process (ID): %d\n", child_pid, parent_pid);
-			exit(0);
-		}
-	wait(NULL);
-	}
 	
 	printf("\nCreate a binary tree of the processes:\n");
 	for (int i = 0; i < 2; i++){
@@ -23,29 +13,17 @@ int main(int argc, char **argv){
 			child_pid = getpid();
 			parent_pid = getppid();
 			printf("1. Child Process (ID): %d, parent process (ID): %d\n", child_pid, parent_pid);
+
 			if (i == 0) {
 				fork();
 				child_pid = getpid();
 				parent_pid = getppid();
 				printf("2. Child Process (ID): %d, parent process (ID): %d\n", child_pid, parent_pid);
-				wait(NULL);
 			}
 			exit(0);
 		}
 		wait(NULL);
 	}
+	}
 	
-	printf("\nStar topology of processes:\n");
-	for (int i = 0; i < 5; i++){
-		if (fork() == 0) {
-		child_pid = getpid();
-		parent_pid = getppid();
-		printf("Child Process (ID): %d, parent process (ID): %d\n", child_pid, parent_pid);
-	exit(0);
-		}
-	}
-	for (int i = 0; i < 5; i++){
-		wait(NULL);
-	}
-	return 0;
-}
+
--- Part_1/test_exec.cpp
+++ Part_1/test_exec.cpp
@@ -1,11 +1,12 @@
 //test_exec.cpp
 #include <unistd.h>
 #include <iostream>
+
 using namespace std;
-int main(){
+
+int main() {
 	cout << "Running ps with execlp\n";
 	execlp ("ps", "ps", "-ax", 0);
 	cout << "The program is exiting!\n";
-
 	return 0;
 }
