1194c9107c98696d4fd45252ead64c5a8998e563
Benharrity <71051403+Benharrity@users.noreply.github.com>
2023-10-21 19:45:42 -0700

    minor changes

--- Part_3/pthreads_message_demo.cpp
+++ Part_3/pthreads_message_demo.cpp
@@ -1,8 +1,8 @@
 /*
-	pthreads_message_demo.cpp
-	A very simple example demonstrating the usage of pthreads.
-	Compile: g++ -o pthreads_message _demo pthreads_demo.cpp -lpthread
-	Execute: ./pthreads_message _demo
+  pthreads_message_demo.cpp
+  A very simple example demonstrating the usage of pthreads.
+  Compile: g++ -o pthreads_message _demo pthreads_demo.cpp -lpthread
+  Execute: ./pthreads_message _demo
 */
 
 #include<iostream>
@@ -12,18 +12,19 @@
 #include<string>
 
 void *thread_func(void *arg) {
-	char *message = (char *) arg;
-	strcpy(message, "You are in thread function!\n");
-	return NULL;
+  char *message = (char *) arg;
+  strcpy(message, "You are in thread function!\n");
+  return NULL;
 }
 
 int main() {
-	char *message = (char*)malloc(100);
-	strcpy(message, "You are in the main function!\n");
-	pthread_t tid1;
-	pthread_create(&tid1, NULL, &thread_func, message);
-	
-	printf("%s", message);
-	pthread_join(tid1, NULL);
-	return 0;
+  char *message = (char*)malloc(100);
+  strcpy(message, "You are in the main function!\n");
+  pthread_t tid1;
+  pthread_create(&tid1, NULL, &thread_func, message);
+
+  pthread_join(tid1, NULL);
+  printf("%s", message);
+  
+  return 0;
 }
